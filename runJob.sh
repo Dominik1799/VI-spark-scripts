cd5e91e0cd160148aa1b33df3cd3986ba6e27c42
Dominik Horváth <dom.horvath17@gmail.com>
2021-11-21 10:41:51 +0100

    reworked docker compose

--- runJob.sh
+++ runJob.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 /opt/bitnami/spark/bin/spark-submit --master spark://sparkmaster:7077 /data/scripts/main.py && \
+echo Cleaning1 && \
 rm /data/output/main/.*.crc  && \
+echo Concating && \
 cat /data/output/main/*.json > /data/output/main/merged.jl && \
+echo Cleaning2 && \
 rm /data/output/main/*.json  && \
 echo Success!
